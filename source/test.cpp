5b5340b89ae0f892b459d7ac20eafd5ab1921c68
pdrmota <peterdrmota@gmx.at>
2017-09-08 12:11:26 +0100

    Program that calculates Pi by randomly sampling points within a quarter of a 1x1 square and counting the number of points whose distance to the origin is less than 1.

--- source/test.cpp
+++ source/test.cpp
@@ -1,10 +1,16 @@
 #include "mc.hpp"
+#include <iostream>
 
 int main() {
-	mc inst(4);
-	inst.set_rdvec();
-	inst.ret_rdvec(std::cout);
-	std::cout << inst.l2_norm_rdvec() << std::endl;
-
+	mc inst(2);
+	int drinnen{ 0 }, max;
+	std::cout << "Anzahl Punkte: ";
+	std::cin >> max;
+	std::cout << std::endl;
+	for (int i = 0; i < max; i++) {
+		inst.update_rdvec();
+		if (inst.l2_norm_rdvec() < 1.0) drinnen++;
+	}
+	std::cout << "Pi ~ " << 4.0*drinnen / max << std::endl;
 	return 0;
 }
