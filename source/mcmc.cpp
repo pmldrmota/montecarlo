8ca072ef0470ea532fb95f888f734b00b447da9b
pdrmota <peterdrmota@gmx.at>
2017-09-13 22:54:56 +0100

    generalised metrop to a markov chain monte carlo class (virtual propose and virtual success functions) - update uses these functions which will be specified in the inheritant class

---
+++ source/mcmc.cpp
@@ -0,0 +1,44 @@
+#include "mcmc.hpp"
+
+mcmc::mcmc(unsigned int dimension) : mc(dimension) {
+	starting_point();
+	y.resize(dim);
+}
+mcmc::mcmc(const std::vector< std::pair<double, double> > &lims) : mc(lims) {
+	starting_point();
+	y.resize(dim);
+}
+void mcmc::starting_point() {
+	std::pair<double, double> limit;
+	for (int i = 0; i < x.size(); i++) {
+		limit = limits.at(i);
+		x.at(i) = limit.first + spans.at(i)*uniform_dist(gen);
+	}
+	trace.push_back(x);
+}
+void mcmc::propose() {
+	y = x;
+}
+void mcmc::update(const std::vector<std::vector<double>> &observations) {
+	do {
+		propose();
+	} while (!y_inside_space());
+	if (success(observations)) x = y;
+	step_nr++;
+	trace.push_back(x);
+}
+bool mcmc::success(const std::vector<std::vector<double>> &observations) {
+	return true;
+}
+bool mcmc::y_inside_space() {
+	bool inside{ true };
+	std::pair<double, double> limit;
+	for (int i = 0; i < dim; i++) {
+		limit = limits.at(i);
+		if (y.at(i) < limit.first || y.at(i) > limit.second) {
+			inside = false;
+			break;
+		}
+	}
+	return inside;
+}
---
+++ source/mcmc.hpp
@@ -0,0 +1,28 @@
+#ifndef _mcmc_hpp_
+#define _mcmc_hpp_
+
+#include "mc.hpp"
+#include <random>
+#include <tuple>
+#include <vector>
+
+class mcmc : public mc {
+protected:
+// starting point
+	std::uniform_real_distribution<double> uniform_dist;	// needed for starting_point() and success()
+	void starting_point();	// randomly chooses a starting point
+// proposal
+	std::vector<double> y;	// proposed next position
+	virtual void propose();	// proposes next position. has to be specified in inherited class!
+// accepting proposal
+	virtual bool success(const std::vector<std::vector<double>> &observations);	// returns 1 if update is accepted, 0 otherwise. has to be specified in inherited class!
+	bool y_inside_space();	// checks if proposed y is inside space
+
+public:
+	mcmc(const unsigned int dim);
+	mcmc(const std::vector< std::pair<double, double> > &lims);
+
+	void update(const std::vector<std::vector<double>> &observations);				// proposes new position and accepts it if success evaluates to true
+};
+
+#endif // !_mcmc_hpp_
