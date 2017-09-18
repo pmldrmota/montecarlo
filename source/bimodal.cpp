d76e5cc23935d245bd9f412b88dabd02255eba85
pdrmota <peterdrmota@gmx.at>
2017-09-18 15:33:58 +0100

    experimented with uniform distribution within limits

--- source/bimodal.cpp
+++ source/bimodal.cpp
@@ -13,8 +13,9 @@ void bimodal::set_proposal_width(const double w) {
 	proposal_width = w;
 }
 double bimodal::log_target_distribution(const double x) {
-	return -0.2*x*x + std::log(0.3 + 0.7*0.0000000021*std::exp(4 * x));
-	//return 0.3*std::exp(-0.2*x*x) + 0.7*std::exp(-0.2*(x - 10)*(x - 10));
+	if (x >= -10 && x <= 24) return 0;
+	else return -100;
+	//return -0.2*x*x + std::log(0.3 + 0.7*0.0000000021*std::exp(4 * x));
 }
 void bimodal::set_log_p_success() {
 	log_p_success = log_target_distribution(y.at(0)) - log_target_distribution(x.at(0));
--- source/bimodal_walk.cpp
+++ source/bimodal_walk.cpp
@@ -10,15 +10,15 @@ int main() {
 	std::cin >> max;
 
 	std::vector<std::pair<double, double>> lims;
-	lims.push_back(std::pair<double, double>(-20, 30));	// limits of variable 0
+	lims.push_back(std::pair<double, double>(-50, 50));	// limits of variable 0
 
 	bimodal inst(lims);
 	inst.set_proposal_width(5);
-	inst.burn_in(500);
+	inst.burn_in(200);
 
 	for (int i = 0; i < max; i++) inst.update();
 
-	unsigned int bins{ 100 };
+	unsigned int bins{ 50 };
 	std::cout << "Verteilung: " << std::endl;
 	inst.print_histogram(std::cout, bins, 0);
 
