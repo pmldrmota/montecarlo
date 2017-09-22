eed3cba7fad9b8a253a5221aa0f4e86fdbeb2ed2
pdrmota <peterdrmota@gmx.at>
2017-09-22 20:25:33 +0100

    trying the functions write_autocorrelation_to_file() and write_trace_to_file()

--- source/bimodal_walk.cpp
+++ source/bimodal_walk.cpp
@@ -3,6 +3,7 @@
 #include <tuple>
 #include "bimodal.hpp"
 
+
 int main() {
 
 	int max;
@@ -10,17 +11,25 @@ int main() {
 	std::cin >> max;
 
 	std::vector<std::pair<double, double>> lims;
-	lims.push_back(std::pair<double, double>(-50, 50));	// limits of variable 0
+	lims.push_back(std::pair<double, double>(-20, 30));	// limits of variable 0
 
 	bimodal inst(lims);
 	inst.set_proposal_width(5);
-	inst.burn_in(200);
+	inst.burn_in(500);
 
 	for (int i = 0; i < max; i++) inst.update();
 
-	unsigned int bins{ 50 };
+	unsigned int bins{ 100 };
 	std::cout << "Verteilung: " << std::endl;
 	inst.print_histogram(std::cout, bins, 0);
+
+	std::ofstream autoc("autocorrelation.dat");
+	inst.write_autocorrelation_to_file(autoc, 500);
+	autoc.close();
+
+	std::ofstream pointf("points.dat");
+	inst.write_trace_to_file(pointf);
+	pointf.close();
 
 	return 0;
 }
